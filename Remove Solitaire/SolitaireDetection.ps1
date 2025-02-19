a86a1c5a134c7fe94b6ad2fe7cd9b84c160d117f
Sethukumarb <88037170+Sethukumarb@users.noreply.github.com>
2025-02-19 09:40:47 +0530

    Add files via upload

---
+++ Remove Solitaire/SolitaireDetection.ps1
@@ -0,0 +1,12 @@
+﻿# Author: Sethukumar
+
+$app = Get-AppxPackage -Allusers | Where-Object {$_Name -like "*Solitaire*"}
+
+If ($app -ne $null) 
+{
+exit 1
+}
+
+else {
+exit 0
+}
---
+++ Remove Solitaire/SolitaireRemediation.ps1
@@ -0,0 +1,17 @@
+﻿# Author: Sethukumar
+
+$app = Get-AppxPackage -AllUsers | Where-Object {$_.Name -like "*Solitaire*"}
+
+if ($app -ne $null)
+{
+Remove-AppxPackage $app -AllUsers}
+
+timeout /t 30
+
+$app = Get-AppxPackage -AllUsers | Where-Object { $_Name-like "*Solitaire*"}
+
+if ($app -eq $null)
+{exit 0)
+
+else {
+exit 1}
